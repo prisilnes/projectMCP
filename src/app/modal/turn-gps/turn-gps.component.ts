3641cd33aa6da1f9dbd9b6b7f89feb1373878778
FrendyWong192 <frendy.wong98@gmail.com>
2019-12-18 14:13:41 +0700

    GPS TRY

--- src/app/modal/turn-gps/turn-gps.component.ts
+++ src/app/modal/turn-gps/turn-gps.component.ts
@@ -22,7 +22,7 @@ export class TurnGpsComponent implements OnInit {
     private route: Router,
   ) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.checkGps();
   }
 
@@ -34,6 +34,8 @@ export class TurnGpsComponent implements OnInit {
   checkGps() { // ngecek GPS udah nyala atau belum
     this.diagnostic.isLocationEnabled().then((isEnabled) => {
       this.gpsActive = isEnabled;
+      console.log(this.gpsActive);
+      alert(this.gpsActive);
     })
     // this.diagnostic.getLocationMode().then(
     //   (state: any) => {
